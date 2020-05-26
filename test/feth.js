2421b995ca725a162a91cce590bd6c1dab0bd258
seachen <364095436@qq.com>
2020-05-26 11:09:53 +0800

    blob test

--- test/feth.js
+++ test/feth.js
@@ -1,11 +1,18 @@
-const p = new Promise((resolve,reject)=>{
-    resolve()
-})
-let n = 0
-p.then(()=>{
-    return new Promise((resolve,reject)=>{
-        resolve("no")
-    })
-}).then((res)=>{
-    console.log("then",res)
-})
+var assert = require('assert');
+describe('ArrayBuffer to uint16', function() {
+    //双字节
+    let uintBuffer = new ArrayBuffer(4)
+    let u16 = new DataView(uintBuffer)
+
+    //输入内容的位置、输入的值、是否使用小端方式(默认大端）
+    u16.setUint32(0,0x7FFFFFFF,false)
+
+    var b = new Blob(uintBuffer,"application/octet-stream")
+
+
+    
+
+    
+    //获得值
+    console.log(u16.getUint32(0))
+});
